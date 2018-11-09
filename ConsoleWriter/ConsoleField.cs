8a667f1c2db4b468bd4a24e75ef1f247c99d227a
Mikkel Jørgensen <mikk149c@edu.eal.dk>
2018-11-09 08:45:50 +0100

    *

---
+++ ConsoleWriter/ConsoleField.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleWriter
+{
+	public class ConsoleField
+	{
+		private char character = ' ';
+		private ConsoleColor forgroundColor = ConsoleColor.White;
+		private ConsoleColor backgroundColor = ConsoleColor.Black;
+		public char Character { get { return character; } set { character = value; } }
+		public ConsoleColor ForgoundColor { get { return forgroundColor; } set { forgroundColor = value; } }
+		public ConsoleColor BackgoundColor { get { return backgroundColor; } set { backgroundColor = value; } }
+	}
+}
---
+++ ConsoleWriter/Frame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleWriter
+{
+	public class Frame
+	{
+		private List<List<ConsoleField>> consoleFields = new List<List<ConsoleField>>();
+		ConsoleField selection;
+		public ConsoleField Selection { get { return selection; } set { selection = value; } }
+
+		public void Select(int x, int y)
+		{
+			for (int i = consoleFields.Count; i < x+1; i++)
+				consoleFields.Add(new List<ConsoleField>());
+			for (int i = consoleFields[x].Count; i <= y+1; i++)
+				consoleFields[x].Add(new ConsoleField());
+			selection = consoleFields[x][y];
+		}
+	}
+}
